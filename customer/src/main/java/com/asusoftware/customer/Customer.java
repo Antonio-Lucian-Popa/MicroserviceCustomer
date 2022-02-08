9246684a34a2a7e384b3807898722f642fffe312
AsuSoftware <asu.software.dev@gmail.com>
2022-02-08 19:13:22 +0200

    Initial commit

---
+++ customer/src/main/java/com/asusoftware/customer/Customer.java
@@ -0,0 +1,24 @@
+package com.asusoftware.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Entity
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
